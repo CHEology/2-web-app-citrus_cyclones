f936d5a87d205c647a99dbbede1c09bd9b8a801d
chasecvitale <chasevitale@gmail.com>
2026-02-22 22:39:19 -0500

    fixed app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("header.html", title="Bookmarks")
+    return render_template()
 
 if __name__ == "__main__":
     app.run(debug=True)
